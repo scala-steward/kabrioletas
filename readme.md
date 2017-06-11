09c24a871411c0e8bce4dc78025a0d8cbe1f33f4
Martynas Mickevičius <martynas@2m.lt>
2017-06-11 11:06:17 +0300

    Add a microsite

---
+++ readme.md
@@ -0,0 +1,5 @@
+[![Build Status](https://travis-ci.org/2m/kabrioletas.svg?branch=master)](https://travis-ci.org/2m/kabrioletas) [![Follow](https://img.shields.io/twitter/follow/kabrioletas.svg?style=social&label=Follow)](https://twitter.com/kabrioletas)
+
+## Kabrioletas
+
+Kabrioletas is a twitter bot that lets you know when you can get that sweet roofless ride.
--- src/main/scala/Kabrioletas.scala
+++ src/main/scala/Kabrioletas.scala
@@ -1,4 +1,20 @@
-package lt.dvim.citywas.kabrioletas
+/*
+ * Copyright 2017 2m
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lt.dvim.citywasp.kabrioletas
 
 import akka.actor.SupervisorStrategy.Resume
 import akka.actor.{Actor, ActorLogging, ActorSystem, OneForOneStrategy, Props}
@@ -95,7 +111,7 @@ class Supervisor extends Actor {
   }
 
   override def preStart() = {
-    context.actorOf(Props[CabrioCheck], "cabrio")
+    context.actorOf(Props[CabrioCheck], "cabrioCheck")
   }
 
   def receive = {
