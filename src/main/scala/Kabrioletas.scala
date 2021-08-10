fe9da51830f939dc3f4c1ce9fab8e74575653bde
Martynas Mickevičius <self@2m.lt>
2021-08-10 21:33:43 +0300

    Upgrade to citywasp-api 2.0

--- src/main/scala/Kabrioletas.scala
+++ src/main/scala/Kabrioletas.scala
@@ -18,6 +18,12 @@ package lt.dvim.citywasp.kabrioletas
 
 import java.time.{Duration, Instant}
 
+import scala.compat.java8.DurationConverters._
+import scala.concurrent.Future
+import scala.concurrent.duration.{Duration => _, _}
+import scala.jdk.CollectionConverters._
+import scala.util.{Failure, Random}
+
 import akka.actor.SupervisorStrategy.Resume
 import akka.actor.{Actor, ActorLogging, ActorSystem, OneForOneStrategy, Props}
 import akka.http.scaladsl.Http
@@ -25,20 +31,26 @@ import akka.http.scaladsl.model.Uri.Query
 import akka.http.scaladsl.model.{HttpRequest, Uri}
 import akka.http.scaladsl.unmarshalling.Unmarshal
 import akka.pattern.pipe
-import akka.stream.ActorMaterializer
-import citywasp.api._
-import com.danielasfregola.twitter4s.TwitterRestClient
-import com.danielasfregola.twitter4s.entities.{RatedData, Tweet}
-import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
-import io.circe._
 
-import scala.compat.java8.DurationConverters._
-import scala.concurrent.duration.{Duration => _, _}
-import scala.util.{Failure, Random}
+import cats.implicits._
+import com.danielasfregola.twitter4s.TwitterRestClient
+import com.danielasfregola.twitter4s.entities.Tweet
+import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
+import eu.timepit.refined._
+import eu.timepit.refined.auto._
+import io.circe._
+import sttp.client3.akkahttp.AkkaHttpBackend
+import sttp.model.{Uri => SttpUri}
+import sttp.tapir.client.sttp.SttpClientInterpreter
+
+import lt.dvim.citywasp.api.Model._
+import lt.dvim.citywasp.api._
+import lt.dvim.citywasp.kabrioletas.ResponseOps._
 
 object CabrioCheck {
   case object DoTheCheck
-  case class ParkedCars(cars: Seq[Car])
+  case object Tweeted
+  case class ParkedCars(cars: List[Car])
   case class LastTweetAndCar(tweets: List[Tweet], car: Option[Car])
   case class CarWithLocation(car: Car, location: OpenCageData.Location)
 }
@@ -48,43 +60,50 @@ class CabrioCheck extends Actor with ActorLogging {
   import context.dispatcher
 
   implicit val sys = context.system
-  implicit val mat = ActorMaterializer()
-
-  val config = context.system.settings.config.getConfig("citywasp")
-  implicit val cw = RemoteCityWasp(config)
+  val backend = AkkaHttpBackend.usingActorSystem(sys)
 
   val twitter = TwitterRestClient()
+
+  final val DryRun = context.system.settings.config.getBoolean("dry-run")
+  final val CitywaspAppVersion = {
+    val str = context.system.settings.config.getString("citywasp.app-version")
+    val refined: Either[String, AppVersion] = refineV(str)
+    refined.getOrElse(throw new Exception(s"unable to parse [$str] to a version"))
+  }
+  final val CitywaspUris =
+    context.system.settings.config.getStringList("citywasp.uris").asScala.toList.map(SttpUri.unsafeParse)
   final val OpenCageDataKey = context.system.settings.config.getString("opencagedata.key")
-  final val CardIdToSearch = context.system.settings.config.getInt("kabrioletas.car-id")
+  final val ServiceIdToSearch = context.system.settings.config.getInt("kabrioletas.service-id")
   final val PollInterval = context.system.settings.config.getDuration("kabrioletas.poll-interval").toScala
 
   var lastTweetAt: Instant = _
 
-  context.system.scheduler.schedule(0.seconds, PollInterval, self, DoTheCheck)
+  context.system.scheduler.scheduleAtFixedRate(0.seconds, PollInterval, self, DoTheCheck)
 
-  override def preStart() = {
+  override def preStart() =
     resetLastTweetTimer()
-  }
 
   def receive = {
     case DoTheCheck =>
       log.info("Starting to look for a wanted car.")
-      cw.login.pipeTo(self)
-    case login: LoggedIn =>
-      log.info("Successfully logged in!")
-      login.availableCars.map(ParkedCars).pipeTo(self)
+      allCars().pipeTo(self)
+      ()
     case ParkedCars(cars) =>
-      val car = cars.find(_.id == CardIdToSearch)
+      log.info(s"Currently total ${cars.size} cars available.")
+      val car = cars.find(_.serviceId == ServiceIdToSearch)
       log.info(s"Car search resulted in $car")
       twitter.homeTimeline(count = 1).map(timeline => LastTweetAndCar(timeline.data.toList, car)).pipeTo(self)
+      ()
     case LastTweetAndCar(Nil, None) =>
       log.info(s"No tweets and no car. Keep on searching...")
       if (Duration.between(Instant.now, lastTweetAt).toDays.abs >= 1) {
         tweetAboutSearch().pipeTo(self)
       }
+      ()
     case LastTweetAndCar(Nil, Some(car)) =>
       log.info(s"Found a car. It is gonna be a great first tweet!")
       reverseGeocodeCarLocation(car).map(CarWithLocation(car, _)).pipeTo(self)
+      ()
     case LastTweetAndCar(tweet :: _, Some(car)) =>
       if (!tweet.text.contains("ready")) {
         log.info(
@@ -92,8 +111,10 @@ class CabrioCheck extends Actor with ActorLogging {
         )
         reverseGeocodeCarLocation(car).map(CarWithLocation(car, _)).pipeTo(self)
       } else if (
-        tweet.coordinates.isDefined && math.abs(tweet.coordinates.get.coordinates.head - car.lon) > 0.001 && math
-          .abs(tweet.coordinates.get.coordinates.tail.head - car.lat) > 0.001
+        tweet.coordinates.isDefined && math.abs(
+          tweet.coordinates.get.coordinates.head - car.long.toDouble
+        ) > 0.001 && math
+          .abs(tweet.coordinates.get.coordinates.tail.head - car.lat.toDouble) > 0.001
       ) {
         log.info(
           s"Found a car [$car] and last tweet was about a parked car [${tweet.text}], but it was on a different place. Let tell the world about the car we just found!"
@@ -102,6 +123,7 @@ class CabrioCheck extends Actor with ActorLogging {
       } else {
         log.info(s"Found a car [$car] and last tweet was about a parked car [${tweet.text}] at the same place.")
       }
+      ()
     case LastTweetAndCar(tweet :: _, None) =>
       if (tweet.text.contains("ready")) {
         log.info(s"No car and last tweet was about a parked car [${tweet.text}]. Tweet about taken car.")
@@ -112,10 +134,12 @@ class CabrioCheck extends Actor with ActorLogging {
           tweetAboutSearch().pipeTo(self)
         }
       }
-    case cwl @ CarWithLocation(car, location) =>
+      ()
+    case cwl @ CarWithLocation(_, location) =>
       log.info(s"Reverse geocoded car location to $location")
       tweetAbout(cwl).pipeTo(self)
-    case t: Tweet =>
+      ()
+    case Tweeted =>
       log.info("Tweet success.")
       resetLastTweetTimer()
     case m =>
@@ -127,7 +151,17 @@ class CabrioCheck extends Actor with ActorLogging {
         case akka.actor.Status.Failure(ex) =>
           log.error(ex.getMessage)
           ex.printStackTrace()
+        case _ => log.error(m.toString)
       }
+  }
+
+  def allCars() =
+    CitywaspUris.map(cars(CitywaspAppVersion)).combineAll.map(ParkedCars.apply)
+
+  def cars(appVersion: AppVersion)(uri: SttpUri) = {
+    val params = Params.default.copy(appVersion = appVersion, country = Country.fromUri(uri))
+    val carsRequest = SttpClientInterpreter().toRequest(Api.CarsLive.GetAvailableCars, Some(uri)).apply(params)
+    carsRequest.send(backend).flatMap(_.toFuture)
   }
 
   def reverseGeocodeCarLocation(car: Car)(implicit sys: ActorSystem) = {
@@ -137,7 +171,7 @@ class CabrioCheck extends Actor with ActorLogging {
       .singleRequest(
         HttpRequest(
           uri = Uri("http://api.opencagedata.com/geocode/v1/json")
-            .withQuery(Query("q" -> s"${car.lat},${car.lon}", "key" -> OpenCageDataKey))
+            .withQuery(Query("q" -> s"${car.lat},${car.long}", "key" -> OpenCageDataKey))
         )
       )
       .flatMap(resp => Unmarshal(resp.entity).to[Location])
@@ -152,29 +186,33 @@ class CabrioCheck extends Actor with ActorLogging {
     } yield s"$suburb$city"
 
     val locationDescription = cityDescription.orElse(location.town).map(location => s" in $location").getOrElse("")
+    val tweet =
+      f"\uD83D\uDE95\uD83D\uDE95\uD83D\uDE95 Parked and ready for a new adventure$locationDescription. Pick me up! https://www.google.com/maps?q=${car.lat}%.6f,${car.long}%.6f ($randomMarker)"
 
-    twitter.createTweet(
-      status =
-        f"\uD83D\uDE95\uD83D\uDE95\uD83D\uDE95 Parked and ready for a new adventure$locationDescription. Pick me up! https://www.google.com/maps?q=${car.lat}%.6f,${car.lon}%.6f ($randomMarker)",
-      latitude = Some(car.lat.toLong),
-      longitude = Some(car.lon.toLong),
-      display_coordinates = true
-    )
+    if (DryRun) {
+      log.info(s"Would tweet: $tweet")
+    } else {
+      twitter.createTweet(
+        status = tweet,
+        latitude = Some(car.lat.toLong),
+        longitude = Some(car.long.toLong),
+        display_coordinates = true
+      )
+    }
+    Future.successful(Tweeted)
   }
 
-  def tweetAboutNoCar() = {
+  def tweetAboutNoCar() =
     twitter.createTweet(
       status =
         s"\uD83D\uDD1C\uD83D\uDD1C\uD83D\uDD1C I am on a ride right now. Will let you know when I am free! ($randomMarker)"
     )
-  }
 
-  def tweetAboutSearch() = {
+  def tweetAboutSearch() =
     twitter.createTweet(
       status =
         s"🔎🔎🔎 There has been no available car for quite some time now. Nevertheless, I keep on searching. Stay tuned! ($randomMarker)"
     )
-  }
 
   def randomMarker =
     Random.alphanumeric.take(6).mkString
@@ -185,12 +223,13 @@ class CabrioCheck extends Actor with ActorLogging {
 
 class Supervisor extends Actor {
   override def supervisorStrategy =
-    OneForOneStrategy(loggingEnabled = true) { case t =>
+    OneForOneStrategy(loggingEnabled = true) { case _ =>
       Resume
     }
 
   override def preStart() = {
-    context.actorOf(Props[CabrioCheck], "cabrioCheck")
+    context.actorOf(Props[CabrioCheck](), "cabrioCheck")
+    ()
   }
 
   def receive = { case _ =>
@@ -199,7 +238,7 @@ class Supervisor extends Actor {
 
 object Kabrioletas extends App {
   val sys = ActorSystem("Kabrioletas")
-  sys.actorOf(Props[Supervisor], "supervisor")
+  sys.actorOf(Props[Supervisor](), "supervisor")
 }
 
 object OpenCageData extends FailFastCirceSupport {
@@ -208,9 +247,9 @@ object OpenCageData extends FailFastCirceSupport {
   implicit val decodeLocation: Decoder[Location] = new Decoder[Location] {
     final def apply(c: HCursor): Decoder.Result[Location] =
       for {
-        suburb <- c.downField("results").downArray.first.downField("components").downField("suburb").as[Option[String]]
-        city <- c.downField("results").downArray.first.downField("components").downField("city").as[Option[String]]
-        town <- c.downField("results").downArray.first.downField("components").downField("town").as[Option[String]]
+        suburb <- c.downField("results").downArray.downField("components").downField("suburb").as[Option[String]]
+        city <- c.downField("results").downArray.downField("components").downField("city").as[Option[String]]
+        town <- c.downField("results").downArray.downField("components").downField("town").as[Option[String]]
       } yield Location(suburb, city, town)
   }
 }
